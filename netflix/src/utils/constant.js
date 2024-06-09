888c24d2cd9f49e5411d72a73d97f47cf9bb8de2
Dipansh28 <dipanshhirudkar786@gmail.com>
2024-06-09 11:24:24 +0530

    123

--- netflix/src/utils/constant.js
+++ netflix/src/utils/constant.js
@@ -1,11 +1,11 @@
-export const API_END_POINT = "https://netflix-project-wome.onrender.com/api/v1/user";
+export const API_END_POINT = "https://flexit-project-b.onrender.com/api/v1/user";
 
 export const options = {
     headers: {
       accept: 'application/json',
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZjM0ZDFhN2Q1ZDJjY2VkMGY0MTY0ZGY1YjE0NGUyYSIsInN1YiI6IjY1ZmIyYTQwNTQ1MDhkMDE3Y2Y4ZTA2YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Lsth_Y0wgLmKm_44gFJ4O9IrOLbCJQZXUOQDAM7lvWo'
     }
-  };
+  };  
   //Movies links
   export const Now_Playing_Movies = "https://api.themoviedb.org/3/movie/now_playing";
   export const Popular_Movie = "https://api.themoviedb.org/3/movie/popular";
